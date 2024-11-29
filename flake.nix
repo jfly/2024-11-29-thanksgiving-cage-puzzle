5eeb7b3cae330d3c1cff1b6c176a4b9b7495c330
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-11-29 14:53:51 -0600

    Init a new rust project

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Solving a puzzle Lucas brought over for Thanksgiving";
+
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.devshell.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            programs.nixfmt.enable = true;
+            programs.rustfmt.enable = true;
+          };
+          devshells.default = {
+            packages = [
+              pkgs.cargo
+              pkgs.rustc
+              pkgs.gcc
+            ];
+          };
+        };
+    };
+}
